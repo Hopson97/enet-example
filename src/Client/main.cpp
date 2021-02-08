91577a51aa80c6389432cffd1302aa31fafe22c7
Hopson97 <mhopson@hotmail.co.uk>
2021-02-08 11:22:46 +0000

    Add imgui

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -1,30 +1,38 @@
 #include "Client.h"
 #include "Keyboard.h"
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Vertex.hpp>
 #include <SFML/Network/IpAddress.hpp>
+#include <imgui/imgui.h>
+#include <imgui_sfml/imgui-SFML.h>
 
 int main()
 {
+    // Init ENET library and connect the client
     if (enet_initialize() != 0) {
         return EXIT_FAILURE;
     }
-
     Client client;
     if (!client.connectTo(sf::IpAddress::getLocalAddress().toString())) {
         return -1;
     }
 
+    // Set up the window and
     sf::RenderWindow window({1280, 720}, "SFML");
     window.setFramerateLimit(60);
     window.setKeyRepeatEnabled(false);
 
-    Keyboard keyboard;
+    // Start imgui
+    ImGui::SFML::Init(window);
 
+    // Start the main game loop
+    Keyboard keyboard;
     sf::Clock dt;
     while (window.isOpen()) {
         sf::Event e;
         while (window.pollEvent(e)) {
             keyboard.update(e);
+            ImGui::SFML::ProcessEvent(e);
             switch (e.type) {
                 case sf::Event::Closed:
                     window.close();
@@ -44,17 +52,23 @@ int main()
                     break;
             }
         }
-
+        // Update
         client.tick();
+        ImGui::SFML::Update(window, dt.restart());
+
+        ImGui::ShowDemoWindow();
 
         if (!client.isConnected()) {
             window.close();
             break;
         }
 
+        // Render
         window.clear();
+        ImGui::SFML::Render(window);
         window.display();
     }
 
+    ImGui::SFML::Shutdown();
     enet_deinitialize();
 }
